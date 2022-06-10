ddcb0c700902d91d0fc00719fbc6281b182d5501
wpaq <wallysonpabloo@gmail.com>
2022-06-10 18:57:02 -0300

    ensure TokenGenerator returns a token if JWT returns token

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,6 +1,8 @@
+const jwt = require('jsonwebtoken')
+
 class TokenGenerator {
     async generate (id) {
-        return null
+        return jwt.sign(id, 'secret')
     }
 }
 
@@ -8,7 +10,14 @@ class TokenGenerator {
 describe('Token Generator', () => {
     test('Should return null if JWT returns null', async () => {
        const sut = new TokenGenerator()
+       jwt.token = null
        const token = await sut.generate('any_id')
        expect(token).toBeNull()
     })
+
+    test('Should return a token if JWT returns token', async () => {
+        const sut = new TokenGenerator()
+        const token = await sut.generate('any_id')
+        expect(token).toBe(jwt.token)
+     })
 })

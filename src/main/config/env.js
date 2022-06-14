ca5288e1e1787c37f0f06838dcae81ffe6359520
wpaq <wallysonpabloo@gmail.com>
2022-06-14 20:04:13 -0300

    add routes file'

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:xxxx/clean-node-api'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api'
 }
